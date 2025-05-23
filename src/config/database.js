c8597b270bf9ffc5d74649fbb568408d7fa83d9b
Gabriel Richter Reynoso <garey91@gmail.com>
2025-05-23 13:26:53 +0200

    Refactor database configuration to use PostgreSQL and update package dependencies

--- src/config/database.js
+++ src/config/database.js
@@ -3,20 +3,16 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-let sequelize;
-
-if (process.env.NODE_ENV !== "test") {
-  sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: process.env.DB_PATH,
-    logging: false, // Set to console.log to see SQL queries
-  });
-} else {
-  sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: ":memory:",
-    logging: false, // Set to console.log to see SQL queries
-  });
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'postgres',
+  process.env.DB_USER || 'postgres',
+  process.env.DB_PASSWORD || 'postgres',
+  {
+    host: process.env.DB_HOST || 'db',
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
+    logging: false, // eller true om du vill se SQL
+  }
+);
 
 module.exports = sequelize;
